d642d357703d5477e5f9a0e04acf146fb488690b
kleber-matos <klebermatos2019@gmail.com>
2024-09-03 12:43:50 -0300

    :bulb: adicionando comentarios das funcionalidades do backend

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,31 +1,32 @@
 // server.mjs
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors"; // Importa o middleware CORS
+import express from "express"; // Importa o framework Express para facilitar a criação do servidor web
+import bodyParser from "body-parser"; // body-parser transforma esse JSON em um objeto JavaScript para que você possa utilizá-lo na sua aplicação.
+import cors from "cors"; // Importa o middleware CORS para permitir que o servidor aceite requisições de diferentes origens
 
-const app = express();
-const port = 3001; // Atualize a porta para 3001, conforme o erro indica
+const app = express(); // Cria uma instância do Express
+const port = 3001; // Define a porta em que o servidor vai rodar (3001)
 
-// Configura o middleware CORS
-app.use(cors()); // Permite requisições de qualquer origem
+app.use(cors()); // Configura o CORS para permitir requisições de qualquer origem
 
-// Middleware para analisar o corpo das requisições
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // Configura o middleware para analisar o corpo das requisições no formato JSON
 
-let pedidos = [];
+let pedidos = []; // Array que armazenará os pedidos recebidos
 
-// Rota GET
+// Define uma rota GET para retornar a lista de pedidos
 app.get("/", (req, res) => {
-  res.json(pedidos);
+  res.json(pedidos); // Retorna a lista de pedidos em formato JSON
 });
 
-// Rota POST
+// Define uma rota POST para adicionar um novo pedido
 app.post("/", (req, res) => {
-  const novoPedido = req.body;
-  pedidos.push(novoPedido);
-  res.status(201).json(novoPedido);
+  const novoPedido = req.body; // Obtém o novo pedido do corpo da requisição
+  pedidos.push(novoPedido); // Adiciona o novo pedido ao array de pedidos
+  res.status(201).json(novoPedido); // Retorna o novo pedido com o status HTTP 201 (Criado)
 });
 
+// Inicia o servidor na porta especificada e exibe uma mensagem no console
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em http://localhost:${port}`); // Informa que o servidor está rodando na porta 3001
 });
+
+// node server.mjs
